c05e750964dac31465ad9a9fed647ca257d36cef
Nicolas <nicolasfreire2005@gmail.com>
2024-10-25 15:27:20 -0300

    Adição do novo código Javascript, com manipulação do DOM. Adição das constantes necessárias para realização das funções do código.

--- Página Dinâmica/Página Dinâmica/js/script.js
+++ Página Dinâmica/Página Dinâmica/js/script.js
@@ -1,5 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
 
+    const get = { method: 'GET' };
+    const url = 'https://jsonplaceholder.typicode.com/';
+    const posts = 'posts/1';
+    const clientData = document.getElementById('infoClient');
+    const infoDisplay = document.getElementById('infoDisplay');
 
-
-
+});
 
